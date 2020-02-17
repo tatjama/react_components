7b384c7f3bc16b4b5544b3b6f710dddc2388ddf2
tatjama <tanja120a@gmail.com>
2020-02-17 15:11:40 +0100

    Import FOOS stylesheet semantic-ui, created first comment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 const App = ()=> {
-    return(<div>React components</div>)
+    return(
+    <div className = "ui comments">    
+        <div className ="comment">
+            <a className=" avatar" href="/">
+                <img alt="avatar" src="images/avatar/small/matt.jpg"/>
+            </a>
+            <div className = "content">
+                 <a className = "author" href="/">Sam</a>
+                <div className = "metadata">
+                    <span className = "date">Today 06:00h
+                    </span>
+                </div>
+                <div className = "text">Nice blog!</div>
+            </div>
+        </div>
+    </div>
+    )
+    
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
