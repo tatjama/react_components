09b46c78920cf14a3c10128c9ca11d43a965d535
tatjama <tanja120a@gmail.com>
2020-02-17 11:41:00 +0100

    Create React app and published it to the GitHub Pages

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReacrDOM from 'react-dom';
+
+const App = () =>{
+    return(<div>React components</div>)
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
